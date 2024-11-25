bdad8693f6f810298db6b70954202eb3a7180a79
kevo8763 <kipkirui8763@gmail.com>
2024-11-25 17:36:34 +0300

    intial commit

---
+++ src/App/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use Framework\TemplateEngine;
+
+class HomeController
+{
+    private TemplateEngine $view;
+
+    public function __construct()
+    {
+        $this->view = new TemplateEngine();
+    }
+
+    public function home()
+    {
+        dd($this->view);
+        echo 'home page';
+    }
+}
--- src/Framework/App.php
+++ src/Framework/App.php
@@ -5,8 +5,21 @@ declare(strict_types=1);
 namespace Framework;
 
 class App {
-    public function run() {
-        echo "Application is running";
+    private Router $router;
+
+    public function __construct()
+    {
+        $this->router = new Router();
     }
 
+    public function run() {
+        $path = parse_url($_SERVER['REQUEST_URL'], PHP_URL_PATH);
+        $method = $_SERVER['REQUST_METHOD'];
+
+        $this->router->dispatch($path, $method);
+    }
+
+    public function get(string $path, array $controller) {
+        $this->router->add('GET', $path, $controller);
+    }
 }
